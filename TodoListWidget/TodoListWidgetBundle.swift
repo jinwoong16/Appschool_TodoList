ae536c5b94ab41fa78297d5bf3f8571abb20e867
jinwoong <baseball0020@gmail.com>
2024-04-25 18:39:19 +0900

    chore: add widget target

---
+++ TodoListWidget/TodoListWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TodoListWidgetBundle.swift
+//  TodoListWidget
+//
+//  Created by jinwoong Kim on 4/25/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TodoListWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TodoListWidget()
+    }
+}
