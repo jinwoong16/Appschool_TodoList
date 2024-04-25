8dc93bdff07d3968af05b09a39a2353494f53c0b
jinwoong <baseball0020@gmail.com>
2024-04-25 13:35:17 +0900

    feat: add presentation view

---
+++ TodoList/Presentation/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  TodoList
+//
+//  Created by jinwoong Kim on 4/25/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
---
+++ TodoList/Presentation/TaskRow.swift
@@ -0,0 +1,18 @@
+//
+//  TaskRow.swift
+//  TodoList
+//
+//  Created by jinwoong Kim on 4/25/24.
+//
+
+import SwiftUI
+
+struct TaskRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskRow()
+}
