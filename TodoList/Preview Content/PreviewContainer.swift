1dc93dcf280e4f9d740c354a4122276d883a6d4a
jinwoong <baseball0020@gmail.com>
2024-04-25 13:33:39 +0900

    test: add `previewContainer` for preview

---
+++ TodoList/Preview Content/PreviewContainer.swift
@@ -0,0 +1,24 @@
+//
+//  PreviewContainer.swift
+//  TodoListTests
+//
+//  Created by jinwoong Kim on 4/25/24.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+let previewContainer: ModelContainer = {
+    do {
+        let container = try ModelContainer(
+            for: Task.self,
+            configurations: ModelConfiguration(isStoredInMemoryOnly: true)
+        )
+        TaskStubs.tasks.forEach { container.mainContext.insert($0) }
+        
+        return container
+    } catch {
+        fatalError("Could not create ModelContainer: \(error)")
+    }
+}()
